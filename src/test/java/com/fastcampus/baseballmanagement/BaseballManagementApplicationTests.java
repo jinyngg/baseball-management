3b182219663f0a8b3a9b31bc7a17a25dc850d790
jyjang <jinyngg@gmail.com>
2023-06-26 13:07:34 +0900

    init : init project (#1)

---
+++ src/test/java/com/fastcampus/baseballmanagement/BaseballManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.baseballmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
