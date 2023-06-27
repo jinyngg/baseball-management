c2069cbb2d6428ebf68cdef386fc5106b268182e
youngjun <sohn919@naver.com>
2023-06-27 15:33:50 +0900

    feat : Stadium 기능 추가 (#3)

--- src/main/java/com/fastcampus/baseballmanagement/core/annotation/AppConfig.java
+++ src/main/java/com/fastcampus/baseballmanagement/core/annotation/AppConfig.java
@@ -1,10 +1,17 @@
 package com.fastcampus.baseballmanagement.core.annotation;
 
+import com.fastcampus.baseballmanagement.stadium.service.StadiumService;
+import com.fastcampus.baseballmanagement.stadium.service.impl.StadiumServiceImpl;
 import com.fastcampus.baseballmanagement.team.service.TeamService;
 import com.fastcampus.baseballmanagement.team.service.impl.TeamServiceImpl;
 
 @MyConfiguration
 public class AppConfig {
+
+    @MyBean
+    public StadiumService stadiumService() {
+        return new StadiumServiceImpl();
+    }
 
     @MyBean
     public TeamService teamService() {
--- src/main/java/com/fastcampus/baseballmanagement/stadium/service/impl/StadiumDAO.java
+++ src/main/java/com/fastcampus/baseballmanagement/stadium/service/impl/StadiumDAO.java
@@ -1,15 +1,99 @@
 package com.fastcampus.baseballmanagement.stadium.service.impl;
 
-import com.fastcampus.baseballmanagement.stadium.Stadium;
-import org.apache.ibatis.annotations.Mapper;
+import com.fastcampus.baseballmanagement.config.DBConnection;
+import com.fastcampus.baseballmanagement.stadium.dto.Stadium;
+import com.fastcampus.baseballmanagement.stadium.dto.StadiumRegistration;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-@Mapper
-interface StadiumDAO {
-    Stadium findById(int id);
-    List<Stadium> findAll();
-    int insert(Stadium stadium);
-    int delete(int id);
-    int update(Stadium stadium);
+public class StadiumDAO {
+
+    private static StadiumDAO stadiumDAO;
+    private Connection connection;
+
+    private StadiumDAO(Connection connection){
+        this.connection = connection;
+    }
+
+    public static StadiumDAO getInstance() {
+        if(stadiumDAO == null)
+            stadiumDAO = new StadiumDAO(DBConnection.getInstance());
+        return stadiumDAO;
+    }
+
+    // SQL DML
+    private final String insertQuery =
+            "INSERT" +
+                    " INTO stadium" +
+                    " (name, created_at)" +
+                    " VALUES" +
+                    " (?, now())"
+            ;
+
+    private final String selectAllQuery =
+            "SELECT" +
+                    " stadium_id, name, created_at" +
+                    " FROM stadium"
+            ;
+
+    public StadiumRegistration registerStadium(String name) {
+        StadiumRegistration response = null;
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+
+            preparedStatement.setString(1, name);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("경기장 등록 성공 : " + name);
+                response = new StadiumRegistration("경기장 등록에 성공했습니다.", null);
+            } else {
+                System.out.println("경기장 등록 실패");
+                response = new StadiumRegistration("경기장 등록에 실패했습니다.", null);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
+    public List<Stadium> getAllStadiums() {
+        List<Stadium> stadiums = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(selectAllQuery);
+
+            while (resultSet.next()) {
+                int stadiumId = resultSet.getInt("stadium_id");
+                String name = resultSet.getString("name");
+                Timestamp createdAt = resultSet.getTimestamp("created_at");
+
+                Stadium stadium = Stadium.builder()
+                        .stadiumId(stadiumId)
+                        .name(name)
+                        .createdAt(createdAt)
+                        .build();
+
+                stadiums.add(stadium);
+            }
+
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return stadiums;
+    }
+
+
+//    Stadium findById(int id);
+//    int delete(int id);
+//    int update(Stadium stadium);
+
 }
