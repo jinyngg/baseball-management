10ec70d7b7208f0d8b77a0dc6355f855ea2ce813
youngjun <sohn919@naver.com>
2023-06-29 17:21:21 +0900

    refactor : Servlet, Scan 코드 리팩토링 (#7)

--- src/main/java/com/fastcampus/baseballmanagement/core/MyDispatcherServlet.java
+++ src/main/java/com/fastcampus/baseballmanagement/core/MyDispatcherServlet.java
@@ -13,102 +13,115 @@ import java.util.Set;
 
 public class MyDispatcherServlet {
 
-
     private final MyConfigurationScan myConfigurationScan = new MyConfigurationScan();
     private final Map<String, Object> myBeanFactory = new HashMap<>();
     private final Map<Object, Map<String, Method>> myExceptionHandlerFactory = new HashMap<>();
+    private static MyDispatcherServlet myDispatcherServlet;
 
-
-    // TODO : 23.06.28
-    //  1. 생성자를 싱글톤으로 바꾸고 호출 시에 init() 을 호출하도록 변경 필요 -> Main 에서 init() 을 할 필요 없도록
-    //  2. configurationInit, exceptionHandlerInit 을 호출해주는 메서드 생성 필요
-    //  3. final 로 선언한 변수들 생성자 호출에 초기화 하도록
-    //  4. 전체적이 코드 리팩토링 필요
-    public void configurationInit() throws Exception {
-        Set<Class<?>> classes = myConfigurationScan.getConfigurationClass();
-        for (Class<?> clazz : classes) {
-            Object instance = clazz.getDeclaredConstructor().newInstance();
-            Method[] methods = clazz.getDeclaredMethods();
-            for (Method method : methods) {
-                MyBean annotation = method.getDeclaredAnnotation(MyBean.class);
-                if (annotation != null) {
-                    Object object = method.invoke(instance);
-                    myBeanFactory.put(object.toString(), object);
-                }
-            }
-        }
-        exceptionHandlerInit();
+    private MyDispatcherServlet() {
+        beanFactoryInit();
+        exceptionHandlerFactoryInit();
     }
 
-    private void exceptionHandlerInit() {
-        for(String objectString : myBeanFactory.keySet()) {
-            Object object = myBeanFactory.get(objectString);
-            Class<?> objectClass = object.getClass();
-            Method[] objectMethods = objectClass.getDeclaredMethods();
-            for (Method objectMethod : objectMethods) {
-                MyExceptionHandler exceptionAnno = objectMethod.getDeclaredAnnotation(MyExceptionHandler.class);
-
-                if(exceptionAnno == null) continue;
-
-                Map<String, Method> getMap = myExceptionHandlerFactory.getOrDefault(object, new HashMap<>());
-                getMap.put(exceptionAnno.value(), objectMethod);
-                myExceptionHandlerFactory.put(object, getMap);
+    public static MyDispatcherServlet getInstance() {
+        if (myDispatcherServlet == null) {
+            myDispatcherServlet = new MyDispatcherServlet();
+        }
+        return myDispatcherServlet;
+    }
+    
+    private void beanFactoryInit() {
+        Set<Class<?>> configurationClass = myConfigurationScan.getConfigurationClass();
+        for (Class<?> clazz : configurationClass) {
+            try {
+                Object instance = clazz.getDeclaredConstructor().newInstance();
+                Method[] methods = clazz.getDeclaredMethods();
+                addBeanFactory(instance, methods);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
 
-    private void findUri(String uri) {
-        for(String objectString : myBeanFactory.keySet()) {
-            Object object = myBeanFactory.get(objectString);
-            Class<?> objectClass = object.getClass();
-            Method[] objectMethods = objectClass.getDeclaredMethods();
-            for(Method objectMethod : objectMethods) {
-                MyRequestMapping anno = objectMethod.getDeclaredAnnotation(MyRequestMapping.class);
-                if(anno != null && anno.value().equals(uri)) {
-                    invokeMethod(object, objectMethod);
-                }
+    private void addBeanFactory(Object instance, Method[] methods) {
+        for (Method method : methods) {
+            MyBean beanAnnotation = method.getDeclaredAnnotation(MyBean.class);
+            if (beanAnnotation == null) {
+                continue;
+            }
+            try {
+                Object object = method.invoke(instance);
+                myBeanFactory.put(object.getClass().getSimpleName(), object);
+            } catch (InvocationTargetException | IllegalAccessException e) {
+                e.printStackTrace();
             }
         }
     }
 
-    private void invokeMethod(Object object, Method objectMethod) {
-        try {
-            objectMethod.invoke(object);
-        }
-        catch (InvocationTargetException | IllegalAccessException exception) {
-            String exceptionCauseName = exception.getCause().getClass().getSimpleName();
-            for(Object exptionObject : myExceptionHandlerFactory.keySet()) {
-                Map<String, Method> exceptionMethod = myExceptionHandlerFactory.get(exptionObject);
-                for(String exceptionName : exceptionMethod.keySet()) {
-                    if (exceptionName.equals(exceptionCauseName)) {
-                        Method method = exceptionMethod.get(exceptionName);
-                        invokeMethod(exptionObject, method);
-                    }
-                }
-            }
+    private void exceptionHandlerFactoryInit() {
+        for (String objectString : myBeanFactory.keySet()) {
+            Object beanObject = myBeanFactory.get(objectString);
+            addExceptionMethod(beanObject);
         }
     }
 
-    private void findUriParam(String uri, Map<String, String> map) throws Exception {
-        for(String objectString : myBeanFactory.keySet()) {
-            Object object = myBeanFactory.get(objectString);
-            Class<?> objectClass = object.getClass();
-            Method[] objectMethods = objectClass.getDeclaredMethods();
-            for(Method objectMethod : objectMethods) {
-                MyRequestMapping anno = objectMethod.getDeclaredAnnotation(MyRequestMapping.class);
-                if(anno != null && anno.value().equals(uri)) {
-                    Parameter[] parameters = objectMethod.getParameters();
-                    Object[] arg = new Object[objectMethod.getParameterCount()];
-                    for(int i=0; i<arg.length; i++) {
-                        String name = parameters[i].getName();
-                        Class<?> type = parameters[i].getType();
-                        String value = map.get(name);
-                        arg[i] = convertTo(value, type);
-                    }
-                    objectMethod.invoke(object, arg);
-                }
+    private void addExceptionMethod(Object beanObject) {
+        Class<?> beanObjectClass = beanObject.getClass();
+        Method[] beanMethods = beanObjectClass.getDeclaredMethods();
+        for (Method method : beanMethods) {
+            MyExceptionHandler exceptionAnno = method.getDeclaredAnnotation(MyExceptionHandler.class);
+            if (exceptionAnno == null) {
+                continue;
+            }
+            Map<String, Method> getExceptionMap = myExceptionHandlerFactory.getOrDefault(beanObject, new HashMap<>());
+            getExceptionMap.put(exceptionAnno.value(), method);
+            myExceptionHandlerFactory.put(beanObject, getExceptionMap);
+        }
+    }
+
+    public void parseUri(String uri) {
+        RequestURI requestURI = new RequestURI(uri);
+        boolean isURI = findUri(requestURI.getUri(), requestURI.getParam());
+        if (!isURI) {
+            System.out.println("잘못된 요청입니다.");
+        }
+    }
+
+    private boolean findUri(String uri, Map<String, String> paramMap) {
+        for (String objectString : myBeanFactory.keySet()) {
+            Object beanObject = myBeanFactory.get(objectString);
+            if (isBeanFactory(uri, paramMap, beanObject)) {
+                return true;
             }
         }
+        return false;
+    }
+
+    private boolean isBeanFactory(String uri, Map<String, String> paramMap, Object beanObject) {
+        Class<?> beanObjectClass = beanObject.getClass();
+        Method[] beanMethods = beanObjectClass.getDeclaredMethods();
+        for (Method method : beanMethods) {
+            MyRequestMapping anno = method.getDeclaredAnnotation(MyRequestMapping.class);
+            if (anno == null || !anno.value().equals(uri)) {
+                continue;
+            }
+            Object[] arg = resolveArgument(paramMap, method);
+            invokeMethod(beanObject, arg, method);
+            return true;
+        }
+        return false;
+    }
+
+    private Object[] resolveArgument(Map<String, String> paramMap, Method objectMethod) {
+        Parameter[] parameters = objectMethod.getParameters();
+        Object[] argument = new Object[objectMethod.getParameterCount()];
+        for (int i=0; i<argument.length; i++) {
+            String name = parameters[i].getName();
+            String value = paramMap.get(name);
+            Class<?> type = parameters[i].getType();
+            argument[i] = convertTo(value, type);
+        }
+        return argument;
     }
 
     private Object convertTo(String value, Class<?> type) {
@@ -117,18 +130,30 @@ public class MyDispatcherServlet {
         return value;
     }
 
-    public void parseUri(String uri) throws Exception {
-        String[] uriParam = uri.split("\\?");
-        if(uriParam.length > 1) {
-            Map<String, String> map = new HashMap<>();
-            String[] params = uriParam[1].split("&");
-            for(String param : params) {
-                String[] values = param.split("=");
-                map.put(values[0], values[1]);
-            }
-            findUriParam(uriParam[0], map);
-        } else {
-            findUri(uri);
+    private void invokeMethod(Object object, Object[] arg, Method objectMethod) {
+        try {
+            objectMethod.invoke(object, arg);
+        } catch (InvocationTargetException | IllegalAccessException exception) {
+            String exceptionCauseName = exception.getCause().getClass().getSimpleName();
+            resolveException(exceptionCauseName);
         }
     }
+
+    private void resolveException(String exceptionCauseName) {
+        for (Object exptionObject : myExceptionHandlerFactory.keySet()) {
+            Map<String, Method> exceptionMethod = myExceptionHandlerFactory.get(exptionObject);
+            for (String exceptionName : exceptionMethod.keySet()) {
+                if (!exceptionName.equals(exceptionCauseName)) {
+                    continue;
+                }
+                Method method = exceptionMethod.get(exceptionName);
+                try {
+                    method.invoke(exptionObject);
+                } catch (InvocationTargetException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
